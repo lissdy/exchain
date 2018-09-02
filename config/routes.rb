d9a2e0fb07e005dde3f183979e7f126e25715e8d
lisa <lissdy2013@gmail.com>
2018-09-02 08:08:18 +0800

    config bower

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  get 'dashboard/admin_index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
