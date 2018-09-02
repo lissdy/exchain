79faf67032332e299d4bb581953acbbf02afa984
lisa <lissdy2013@gmail.com>
2018-09-02 09:21:39 +0800

    Integrate AdminLTE assets to Rails application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,8 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require admin-lte/bootstrap/js/bootstrap
+//= require admin-lte/dist/js/app.js
 //= require_tree .
